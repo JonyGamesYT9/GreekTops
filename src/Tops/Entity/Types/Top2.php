103c38e21e36a6aff0150c7b85946bafafe5a73f
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-06 10:29:04 -0500

    Create Top2.php

---
+++ src/Tops/Entity/Types/Top2.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tops\Entity\Types;
+
+use pocketmine\entity\Human;
+
+/**
+* Class Top2
+* @package Tops\Entity\Types
+*/
+class Top2 extends Human {
+
+  /**
+  * @return string
+  */
+  public function getName(): string {
+    return '';
+  }
+}
