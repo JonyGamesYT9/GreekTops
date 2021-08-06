5254f5db0d79ed8e74dd2666bc684278796083aa
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-06 10:28:42 -0500

    Create Top1.php

---
+++ src/Tops/Entity/Types/Top1.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tops\Entity\Types;
+
+use pocketmine\entity\Human;
+
+/**
+* Class Top1
+* @package Tops\Entity\Types
+*/
+class Top1 extends Human {
+
+  /**
+  * @return string
+  */
+  public function getName(): string {
+    return '';
+  }
+}
